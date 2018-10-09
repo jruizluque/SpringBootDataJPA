32ab5bcf6185fc7553db26d3fea40a3a0ed42d51
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2018-10-08 23:02:22 -0300

    First commit

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/dao/ClienteDaoImpl.java
@@ -0,0 +1,44 @@
+package com.bolsadeideas.springboot.app.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+@Repository("clienteDaoJPA")
+public class ClienteDaoImpl implements IClienteDao {
+
+	@PersistenceContext
+	EntityManager em;
+	
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly=true)
+	@Override
+	public List<Cliente> findAll() {
+		
+		return em.createQuery("from Cliente").getResultList();
+	}
+
+	@Transactional
+	@Override
+	public void save(Cliente cliente) {
+		
+		if (cliente.getId() != null && cliente.getId() > 0) {
+			em.merge(cliente);
+		} else {
+			em.persist(cliente);		
+		}
+	}
+
+	@Override
+	public Cliente findOne(Long id) {
+		
+		return em.find(Cliente.class, id);
+	}
+
+}
