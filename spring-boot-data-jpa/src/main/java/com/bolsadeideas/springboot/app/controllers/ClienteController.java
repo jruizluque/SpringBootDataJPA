de00967ba3d4cdd46037661c0913388a45c0c113
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2018-10-09 22:43:07 -0300

    43. Agregando Eliminar

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -69,4 +69,13 @@ public class ClienteController {
 		status.setComplete();
 		return "redirect:listar";
 	}
+	
+	@RequestMapping(value="/eliminar/{id}")
+	public String eliminar(@PathVariable(value="id") Long id) {
+		
+		if (id > 0) {
+			clienteDao.delete(id);
+		}
+		return "redirect:/listar";
+	}
 }
--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/dao/ClienteDaoImpl.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/dao/ClienteDaoImpl.java
@@ -24,8 +24,8 @@ public class ClienteDaoImpl implements IClienteDao {
 		return em.createQuery("from Cliente").getResultList();
 	}
 
-	@Transactional
 	@Override
+	@Transactional
 	public void save(Cliente cliente) {
 		
 		if (cliente.getId() != null && cliente.getId() > 0) {
@@ -36,9 +36,18 @@ public class ClienteDaoImpl implements IClienteDao {
 	}
 
 	@Override
+	@Transactional(readOnly=true)
 	public Cliente findOne(Long id) {
 		
 		return em.find(Cliente.class, id);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		
+		Cliente cliente = findOne(id);
+		em.remove(cliente);
 	}
 
 }
