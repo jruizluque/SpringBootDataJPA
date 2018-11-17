9061fcd8ab60941713c4307af6f2b899e30f4a3b
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2018-11-17 17:08:42 -0300

    57. Antes de empezar - Demostración y preparando la aplicación

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -46,6 +46,8 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 	
+	private String foto;
+	
 	public Long getId() {
 		return id;
 	}
@@ -75,5 +77,11 @@ public class Cliente implements Serializable {
 	}
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 }
